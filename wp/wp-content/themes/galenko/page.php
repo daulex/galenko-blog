9f1e2e5bd9af300e3dc86ae96acc585f44ac37da
Kirill G <kirillgalenko@gmail.com>
2022-11-28 17:38:10 +0200

    added litespeed plugin, minor fix

--- wp/wp-content/themes/galenko/page.php
+++ wp/wp-content/themes/galenko/page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <main id="main" role="main">
 	<?php while ( have_posts() ) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
 	<?php endwhile; ?>
 </main><!-- /#main -->
