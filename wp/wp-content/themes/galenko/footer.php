6b04655a93419d49d44f581268329839b393552b
Kirill G <kirillgalenko@gmail.com>
2023-01-11 11:59:21 +0200

    header, footer work

--- wp/wp-content/themes/galenko/footer.php
+++ wp/wp-content/themes/galenko/footer.php
@@ -1,4 +1,19 @@
-<footer class="site-footer"></footer>
+<footer class="site-footer">
+  <div class="container">
+    <div class="row">
+      <div class="row__col">
+        <div class="footer-content">
+          <div class="copyright">
+            &copy; <?php echo date('Y'); ?> Kirill Galenko
+          </div>
+          <div class="love">
+            Made with ❤️ in Latvia 🇱🇻
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
--- wp/wp-content/themes/galenko/templates/nav.php
+++ wp/wp-content/themes/galenko/templates/nav.php
@@ -9,34 +9,32 @@ $args = array(
 ); 
 $nav_menu = wp_nav_menu( $args );
 ?>
-<div class="container">
-  <div class="row">
 
   
-  <header class="row__col primary-header">
-    <a href="/" class="logo">
-      <img width="40" height="60" src="/logo-kg-nopadding.svg" alt="<?php bloginfo(); ?>">
-    </a>
+<header class="row__col primary-header">
+  <a href="/" class="logo">
+    <img width="40" height="60" src="/logo-kg-nopadding.svg" alt="<?php bloginfo(); ?>">
+    <span class="kirill">Kirill</span>
+    <span class="galenko">Galenko</span>
+  </a>
 
-    <?php wp_nav_menu( array(
-      'theme_location'  => 'primary',
-      'depth'           => 1, // 1 = no dropdowns, 2 = with dropdowns.
-      'container'       => 'nav',
-      'menu_class'      => 'primary-nav',
+  <?php wp_nav_menu( array(
+    'theme_location'  => 'primary',
+    'depth'           => 1, // 1 = no dropdowns, 2 = with dropdowns.
+    'container'       => 'nav',
+    'menu_class'      => 'primary-nav',
 
-  ) ); ?>
-    <ul id="social-icons">
-      <li>
-        <a href="https://github.com/daulex" target="_blank" title="github">
-          <?php echo $custom_functions->kg_icons('github-alt'); ?>
-        </a>
-      </li>
-      <li>
-        <a href="https://www.instagram.com/kirill.codes/" target="_blank" title="instagram">
-          <?php echo $custom_functions->kg_icons('instagram'); ?>
-        </a>
-      </li>
-    </ul>
-  </header>
-  </div>
-</div>
+) ); ?>
+  <ul id="social-icons">
+    <li>
+      <a href="https://github.com/daulex" target="_blank" title="github">
+        <?php echo $custom_functions->kg_icons('github-alt'); ?>
+      </a>
+    </li>
+    <li>
+      <a href="https://www.instagram.com/kirill.codes/" target="_blank" title="instagram">
+        <?php echo $custom_functions->kg_icons('instagram'); ?>
+      </a>
+    </li>
+  </ul>
+</header>
