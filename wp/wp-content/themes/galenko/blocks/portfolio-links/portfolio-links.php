5c4643bd93a2ad7cc2bd78f61eb7214dd448227e
Kirill G <kirillgalenko@gmail.com>
2023-01-22 20:47:04 +0200

    portfolio links block and my first portfolio item

---
+++ wp/wp-content/themes/galenko/blocks/portfolio-links/portfolio-links.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Portfolio links Block Template.
+ *
+ * @param	 array $block The block settings and attributes.
+ * @param	 string $content The block inner HTML (empty).
+ * @param	 bool $is_preview True during AJAX preview.
+ * @param	 (int|string) $post_id The post ID this block is saved to.
+ */
+
+if( isset( $block['data']['preview_image_help'] )  ) :
+	echo Awave_Gutenberg::get_preview_image( $block['data']['preview_image_help'], $block['name'] );
+	return;
+endif;
+
+// Create id attribute allowing for custom 'anchor' value.
+$id = 'portfolio-links-' . $block['id'];
+if (!empty($block['anchor'])) :
+	$id = $block['anchor'];
+endif;
+
+// Create class attribute allowing for custom 'className' and 'align' values.
+$className = 'block portfolio-links';
+if (!empty($block['className'])) :
+	$className .= ' ' . $block['className'];
+endif;
+if (!empty($block['align'])) :
+	$className .= ' align' . $block['align'];
+endif;
+
+$className = apply_filters( 'awave_block_class', $className, $block, $post_id );
+
+// Load values and assing defaults.
+$header = get_field('header');
+
+?>
+<section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
+</section>
--- wp/wp-content/themes/galenko/inc/class-galenko-custom-hooks.php
+++ wp/wp-content/themes/galenko/inc/class-galenko-custom-hooks.php
@@ -7,7 +7,7 @@
 class galenko_Custom_Hooks {
 	public function __construct() {
 		add_filter( 'body_class', [$this, 'body_classes'] );
-		// add_action( 'init', [$this, 'generate_custom_cpt'] );
+		add_action( 'init', [$this, 'cpt_project'] );
 		add_action( 'excerpt_more', [$this, 'excerpt_more'] );
 	}
 
@@ -29,56 +29,19 @@ class galenko_Custom_Hooks {
 	 * Example of generating a Custom Post Type.
 	 * Remove and create your own if needed.
 	 */
-	public function generate_custom_cpt()
+	public function cpt_project()
 	{
-		$labels = array(
-			'name'                  => _x( 'Post Types', 'Post Type General Name', 'awave' ),
-			'singular_name'         => _x( 'Post Type', 'Post Type Singular Name', 'awave' ),
-			'menu_name'             => __( 'Post Types', 'awave' ),
-			'name_admin_bar'        => __( 'Post Type', 'awave' ),
-			'archives'              => __( 'Item Archives', 'awave' ),
-			'attributes'            => __( 'Item Attributes', 'awave' ),
-			'parent_item_colon'     => __( 'Parent Item:', 'awave' ),
-			'all_items'             => __( 'All Items', 'awave' ),
-			'add_new_item'          => __( 'Add New Item', 'awave' ),
-			'add_new'               => __( 'Add New', 'awave' ),
-			'new_item'              => __( 'New Item', 'awave' ),
-			'edit_item'             => __( 'Edit Item', 'awave' ),
-			'update_item'           => __( 'Update Item', 'awave' ),
-			'view_item'             => __( 'View Item', 'awave' ),
-			'view_items'            => __( 'View Items', 'awave' ),
-			'search_items'          => __( 'Search Item', 'awave' ),
-			'not_found'             => __( 'Not found', 'awave' ),
-			'not_found_in_trash'    => __( 'Not found in Trash', 'awave' ),
-			'featured_image'        => __( 'Featured Image', 'awave' ),
-			'set_featured_image'    => __( 'Set featured image', 'awave' ),
-			'remove_featured_image' => __( 'Remove featured image', 'awave' ),
-			'use_featured_image'    => __( 'Use as featured image', 'awave' ),
-			'insert_into_item'      => __( 'Insert into item', 'awave' ),
-			'uploaded_to_this_item' => __( 'Uploaded to this item', 'awave' ),
-			'items_list'            => __( 'Items list', 'awave' ),
-			'items_list_navigation' => __( 'Items list navigation', 'awave' ),
-			'filter_items_list'     => __( 'Filter items list', 'awave' ),
-		);
-		$args = array(
-			'label'                 => __( 'Post Type', 'awave' ),
-			'description'           => __( 'Post Type Description', 'awave' ),
-			'labels'                => $labels,
-			'supports'              => false,
-			'hierarchical'          => false,
-			'public'                => true,
-			'show_ui'               => true,
-			'show_in_menu'          => true,
-			'menu_position'         => 5,
-			'show_in_admin_bar'     => true,
-			'show_in_nav_menus'     => true,
-			'can_export'            => true,
-			'has_archive'           => true,
-			'exclude_from_search'   => false,
-			'publicly_queryable'    => true,
-			'capability_type'       => 'page',
-		);
-		register_post_type( 'my_post_type', $args );
+		register_post_type('project',
+      array(
+        'labels'      => array(
+          'name'          => __('Projects', 'awave'),
+          'singular_name' => __('Project', 'awave'),
+        ),
+          'public'      => true,
+          'has_archive' => false,
+          'supports'    => array('title', 'editor', 'thumbnail', 'excerpt', 'featured_image'),
+      )
+    );
 	}
 }
 
